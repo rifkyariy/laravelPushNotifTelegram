dadefdf9b3a565a4303e89d6114434288cbf19c8
rifkyariy <rifkyariy@gmail.com>
2020-12-15 22:20:21 +0700

    add notif

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,19 +4,33 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Notifications\Telegram ;
-use Illuminate\Notifications\Notifiable;
 use App\User;
+use Illuminate\Support\Facades\Notification;
 
 class NotificationController extends Controller
 {
     public function sendToTelegram()
     {
-        $user = User::create([
-            'name' => "n",
-            'email' => "ahsan@apik.com",
-            'password' => "hellomydamnworld",
-        ]);
+        $reciept = '@aritestroom';
 
-        $user->notify(new Telegram());
+        $img = (object)[
+            'receipt' => $reciept, 
+            'type' => 'img',
+            'content' => 'https://file-examples-com.github.io/uploads/2017/10/file_example_JPG_1MB.jpg'
+        ];
+
+        $textButton = (object)[
+            'receipt' => $reciept, 
+            'type' => 'textButton',
+            'content' => (object) [
+                'text' => "*Buku Baru Nih*!\nSudah siap buat pinjem ?",
+                'button' => 'Kunjungi Halaman',
+                'url' => 'https://google.com',
+            ]
+        ];
+
+
+        Notification::send($img ,new Telegram());         
+        Notification::send($textButton ,new Telegram());        
     }
 }
--- app/Notifications/Telegram.php
+++ app/Notifications/Telegram.php
@@ -3,6 +3,8 @@
 namespace App\Notifications;
 use NotificationChannels\Telegram\TelegramChannel;
 use NotificationChannels\Telegram\TelegramMessage;
+use NotificationChannels\Telegram\TelegramFile;
+use NotificationChannels\Telegram\TelegramLocation;
 use Illuminate\Notifications\Notification;
 
 class Telegram extends Notification
@@ -12,10 +14,90 @@ class Telegram extends Notification
         return [TelegramChannel::class];
     }
 
-    public function toTelegram($post)
-    {
-        return TelegramMessage::create()
-            ->to('@aritestroom')
-            ->content("Duar MM");
+    public function toTelegram($data)
+    {   
+        switch ($data->type) {
+            case 'text':
+                $notification = TelegramMessage::create();
+                $notification->content($data->content);
+                break;
+
+            case 'textButton':
+                $notification = TelegramMessage::create();
+                $notification->content($data->content->text);
+                $notification->button($data->content->button, $data->content->url);
+                break;
+
+            case 'img':
+                $notification = TelegramFile::create();
+                $notification->file($data->content, 'photo');
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+        
+        $notification->to($data->receipt);
+        return $notification;
     }
+
+    public function sendMessage($data)
+    {
+        $notification = TelegramMessage::create();
+
+        $notification->content($data['text']);
+        // if($data['text']){
+        // }
+
+        if($data['button']){
+            foreach ($data['button'] as $key => $button) {
+                $notification->button($button['text'], $button['url']);
+            }
+        }   
+
+        return $notification;
+    }
+
+    public function sendFile($file, $type)
+    {
+        $notification = TelegramFile::create();
+
+        switch ($type) {
+            case 'img':
+                $notification->file($file, 'photo');
+                break;
+            
+            case 'document':
+                $notification->document($file['url'], $file['title']);
+                break;
+
+            case 'video':
+                $notification->video($file);
+                break;
+
+            case 'gif':
+                $notification->animation($file);
+                break;
+            
+            default:
+                return 'error';
+                break;
+        }
+        
+        return $notification;
+    }
+
+    public function sendLocation($data) 
+    {
+        $notification = TelegramLocation::create();
+
+        if($data['longitude'] && $data['latitude']){
+            $notification->longitude($data['longitude']);
+            $notification->latitude($data['latitude']);
+        }
+
+        return $notification;
+    }
+
 }
